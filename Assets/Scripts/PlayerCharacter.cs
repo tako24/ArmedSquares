4a25c836b19ceb4f1cb59a3f30e363413f5caf6e
Ivan Sablin <sablin.2001@inbox.ru>
2022-09-14 14:02:38 +0500

    networking movement added

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,13 @@
+using Mirror;
+
+public class PlayerCharacter : NetworkBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        //InputCmd();
+    }
+}
