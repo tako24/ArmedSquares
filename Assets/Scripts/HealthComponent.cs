2acdb1d0d4cb263b8c8ec7c5604bb5e6dae795a1
Ivan Sablin <sablin.2001@inbox.ru>
2022-09-15 12:07:33 +0500

    health component added

---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,32 @@
+using Mirror;
+using System;
+using UnityEngine;
+
+public class HealthComponent : NetworkBehaviour
+{
+    [SerializeField] int _maxHealth = 100;
+    [SyncVar] int _health;
+
+    public delegate void DeadHandler(GameObject instigator);
+    public event DeadHandler OnDead;
+    public delegate void HealthChangedHandler(GameObject instigator, int oldHealth, int newHealth);
+    public event HealthChangedHandler OnHealthChanged;
+
+    private void Start()
+    {
+        _health = _maxHealth;
+    }
+
+    [Server]
+    public void TakeDamage(GameObject instigator, int damage)
+    {
+        var oldHealth = _health;
+        _health = Mathf.Clamp(_health - damage, 0, _maxHealth);
+        OnHealthChanged?.Invoke(instigator, oldHealth, _health);
+
+        if (_health == 0)
+        {
+            OnDead?.Invoke(instigator);
+        }
+    }
+}
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,12 +2,4 @@ using Mirror;
 
 public class PlayerCharacter : NetworkBehaviour
 {
-    private void Start()
-    {
-    }
-
-    private void FixedUpdate()
-    {
-        //InputCmd();
-    }
 }
